1de8832c7170bd84214fa172c4b6bd90a8abce68
Sisi Freeley <liusi33@gmail.com>
2020-10-23 15:00:13 -0500

    client side component libraries and initial files

---
+++ PuzzlePost/client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { useState, useContext } from 'react';
+import { NavLink as RRNavLink } from "react-router-dom";
+import { UserProfileContext } from "../providers/UserProfileProvider";
+
+const Header = () => {
+
+
+}
+export default Header;
